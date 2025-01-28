d907ace23c1a2082bd1c29af6908e49eb2f5d477
ElioFezollari <fezollarielio@gmail.com>
2025-01-28 16:50:53 -0500

    Added updateMatch

--- services/matchService.js
+++ services/matchService.js
@@ -5,10 +5,39 @@ const { toCamelCase } = require("../utilities/utilities");
 
 const updateMatch = async (user, data) => {
     try {
+
+        const { matchId, homeTeam, awayTeam } = data;
+        const query = "SELECT league_id FROM teams WHERE id = $1";
+        const values = [homeTeam.id];
+
+        const result = await pool.query(query, values);
+
+        if (result.rows.length === 0) {
+            throw new AppError(BAD_REQUEST.TEAM_NOT_EXISTS, 404);
+        }
+        const leagueId = result.rows[0].league_id;
+        
+
+        const players = [...homeTeam.players, ...awayTeam.players];
+
+        for (const player of players) {
+            const { id, goals, shots, assists, saves, interceptions, yellowCards, redCard } = player;
+
+            const insertQuery = `
+                INSERT INTO user_stats (user_id, match_id, goals, shots, assists, saves, interceptions, yellow_card, red_card)
+                VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
+            `;
+            const insertValues = [id, matchId, goals, shots, assists, saves, interceptions, yellowCards, redCard];
+
+            await pool.query(insertQuery, insertValues);
+        }
+
+        return leagueId;
     } catch (error) {
-        throw new AppError(`${e.message}` || "Unknown Error", e.statusCode || 500);
+        throw error;
     }
 };
+
 
 module.exports = {
     updateMatch
